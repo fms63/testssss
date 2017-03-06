2515d39bdc524150abfbcae2d6ce13547db0bcf6
fms <fms@fms-macbook.local>
2017-03-06 18:11:50 +0100

    asdasdas

--- testssss/ViewController.swift
+++ testssss/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //dkjdsjkkjhsdf
         
         // Do any additional setup after loading the view, typically from a nib.
     }
